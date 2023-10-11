3baba48e703e68bdc66a480f0ad3983ee0c47f48
IIII IIIIIII <nick.vidovic@web.de>
2023-10-11 23:14:08 +0200

    Update shoppingplanner.py

--- Shopping Planne/shoppingplanner.py
+++ Shopping Planne/shoppingplanner.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 class ShoppingPlanner:
     def __init__(self, root):
@@ -13,6 +14,9 @@ class ShoppingPlanner:
         self.add_button = tk.Button(root, text="Hinzufügen", command=self.add_item)
         self.add_button.grid(row=0, column=1, pady=10, padx=10)
 
+        self.add_button = tk.Button(root, text="Speichern", command=self.save_list)
+        self.add_button.grid(row=2, column=1, pady=10, padx=10)
+
         # Listbox
         self.listbox = tk.Listbox(root)
         self.listbox.grid(row=1, column=0, columnspan=2, padx=10, sticky="nsew")
@@ -21,13 +25,12 @@ class ShoppingPlanner:
         self.entry.bind('<Return>', self.add_item)
         self.listbox.bind('<Delete>', self.delete_selected)
 
-
         # Konfiguriere das Raster, um sich beim Größenändern anzupassen
         root.grid_rowconfigure(1, weight=1)
         root.grid_columnconfigure(0, weight=1)
         root.grid_columnconfigure(1, weight=1)
 
-    def add_item(self,event):
+    def add_item(self,event=None):
         text = self.entry.get()
         if text:
             self.listbox.insert(tk.END, text)
@@ -37,6 +40,13 @@ class ShoppingPlanner:
         selected_index = self.listbox.curselection()
         if selected_index:
             self.listbox.delete(selected_index)
+    def save_list(self):
+        all_items = self.listbox.get(0, tk.END)
+        current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+        filename = current_time + ".txt"
+
+        with open(filename, 'w') as file:
+            file.write("\n".join(all_items))
 
 if __name__ == "__main__":
     root = tk.Tk()
